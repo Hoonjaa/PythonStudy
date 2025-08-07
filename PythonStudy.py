1f1d8388cd46f9286f349e941fc247f27b2990f5
Hoonjaa <gnswp8978@gmail.com>
2025-08-07 13:59:55 +0900

    [Update] 25.08.07
    for

---
+++ PythonStudy.py
@@ -0,0 +1,55 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+circle
+triangle
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    triangle
+NameError: name 'triangle' is not defined
+circle
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    circle
+NameError: name 'circle' is not defined
+>>> 
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+triangle
+Traceback (most recent call last):
+  File "C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py", line 8, in <module>
+    turtle.forwad(40)
+AttributeError: module 'turtle' has no attribute 'forwad'. Did you mean: 'forward'?
+>>> 
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+rectangle
+Traceback (most recent call last):
+  File "C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py", line 15, in <module>
+    turtle.forwad(40)
+AttributeError: module 'turtle' has no attribute 'forwad'. Did you mean: 'forward'?
+>>> 
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+circle
+>>> 
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+rectangle
+Traceback (most recent call last):
+  File "C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py", line 15, in <module>
+    turtle.forwad(40)
+AttributeError: module 'turtle' has no attribute 'forwad'. Did you mean: 'forward'?
+>>> 
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+triangle
+>>> 
+====== RESTART: C:/Users/gnswp/Documents/GitHub/PythonStudy/PythonStudy.py =====
+quit
