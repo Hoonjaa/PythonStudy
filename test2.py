8c61a213f7c549d395d070fe01639268381dbc8a
Hoonjaa <gnswp8978@gmail.com>
2025-08-07 14:09:18 +0900

    [Update] 25.08.07
    function

--- test2.py
+++ test2.py
@@ -1,22 +1,5 @@
-for n in [1, 3, 5, 6]:
-    print(n)
+def Add(x,y):
+    result = x + y
+    print(result)
 
-for c in 'Un ds':
-    print(c)
-
-total = 0
-for i in range(1,100*1):
-    total += i
-print(total)
-
-
-# 원을 3개 그릴건데 for문으로 만들기
-
-import turtle
-
-for x, y, z in [(200,200,50), (-200,-200,30), (100,100,50)]:
-    turtle.penup()
-    turtle.goto(x,y)
-    turtle.pendown()
-    turtle.circle(z)
-    turtle.write(str((x,y)))
+Add(10,20)
